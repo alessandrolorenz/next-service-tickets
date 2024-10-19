bd09584f968419676d176fcff4772e5d3d4c31a5
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2024-10-19 12:16:31 -0300

    fix(build): button1

--- app/page.tsx
+++ app/page.tsx
@@ -314,7 +314,7 @@ const Home = () => {
                         </CardFooter>
                                 <Link
                                     href="/ticket-app"
-                                    className='align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm py-3.5 px-7 rounded-lg bg-white text-blue-gray-900 shadow-md shadow-blue-gray-500/10 hover:shadow-lg hover:shadow-blue-gray-500/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none gap-3 m-auto w-full'
+                                    className='align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm py-3.5 px-7 rounded-lg bg-white text-blue-gray-900 shadow-md shadow-blue-gray-500/10 hover:shadow-lg hover:shadow-blue-gray-500/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none gap-3 m-auto w-full block'
                                     key="Tickets App"
                                 >
                                     Tickets App
